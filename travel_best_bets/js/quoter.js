4238c21f95dc5e0f9902fe0f7d3faf9f7a6f9856
Thomas Beyer <tbeyer@urthecast.com>
2017-02-09 12:08:00 -0800

    code cleanup up and hooking date switches together with night dial

--- travel_best_bets/js/quoter.js
+++ travel_best_bets/js/quoter.js
@@ -1,3 +1,7 @@
+//////////////
+// util
+//////////////
+
 //format utility
 if (!String.prototype.format) {
   String.prototype.format = function() {
@@ -76,6 +80,10 @@ function removeElement(divName, class_str){
 	}
 }
 
+/*
+ * business logic
+ */
+
 var flight_counter = 1;
 var hotel_counter = 1;
 
@@ -85,243 +93,33 @@ var ORIGIN_CITY = "";
 var DEST_CITY = "";
 var NO_ADULTS = 0;
 
-/*
- * HTML form elements
- */
-
-////////////////////////////////////
-// flight
-////////////////////////////////////
-
-
-
-function addFlight(divName){
-  	var newdiv = document.createElement('div');
-  	newdiv.innerHTML = getFlightHTML();
-  	document.getElementById(divName).appendChild(newdiv);
-  	focusFirstInput(newdiv);
-	flight_counter++;
-}
 
 function updateDates(){
-	var src_obj = document.getElementById("ddate".format(flight_counter));
- 	src_obj.onchange = function(){
- 		dst_obj = document.getElementById("adate".format(flight_counter));
-		dst_obj.value = src_obj.value;
-		ret_dep_obj = document.getElementById("rddate".format(flight_counter));
-		NO_OF_NIGHTS = document.getElementById("nights").value;
+	var night_switch = document.getElementById("nights");
+	var ddate = document.getElementById("ddate");
+ 	ddate.onchange = function(){
+ 		adate = document.getElementById("adate");
+		adate.value = ddate.value;
+		var rddate = document.getElementById("rddate");
+		var tmp = moment(adate.value).add(night_switch.value, 'days')
+		rddate.value = tmp.format("YYYY-MM-DD")
+		radate = document.getElementById("radate");
+		radate.value = rddate.value
+	};
+
+	
+ 	night_switch.onchange = function(){
+ 		dst_obj = document.getElementById("adate");
+		ret_dep_obj = document.getElementById("rddate");
 		var tmp = moment(dst_obj.value)
-		tmp = tmp.add(NO_OF_NIGHTS, 'days')
+		tmp = tmp.add(night_switch.value, 'days')
 		ret_dep_obj.value = tmp.format("YYYY-MM-DD")
-		ret_arr_obj = document.getElementById("radate".format(flight_counter));
+		ret_arr_obj = document.getElementById("radate");
 		ret_arr_obj.value = ret_dep_obj.value
 	};
 }
 
-function getFlightHTML(){
-	var flight_str = `<div id="div_flight_{0}">
-	<fieldset>
-    <legend>Flight</legend>
-		<table border=0 id="flight_{0}">
-			<tr>
-				<td>Departure city:</td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>Departure date:</td>
-				<td><input type="date" id="ddate" value={1}></td>
-			</tr>
-			<tr>
-				<td>Departure time:</td>
-				<td><input type=time step="300" id="dtime" value={2}></td>
-			</tr>
-			<tr>
-				<td>Arrival city:</td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>Arrival date:</td>
-				<td><input type="date" id="adate" value={1}></td>
-			</tr>
-			<tr>
-				<td>Arrival time:</td>
-				<td> <input type="time" step="300" id="atime" value={2}></td>
-			</tr>
-			<tr>
-				<td><b>Return flight</td>
-			</tr>
-			<tr>
-				<td>Departure date:</td>
-				<td><input type="date" id="rddate" value={1}></td>
-			</tr>
-			<tr>
-				<td>Departure time:</td>
-				<td><input type=time step="300" id="rdtime" value={2}></td>
-			</tr>
-			<tr>
-				<td>Arrival date:</td>
-				<td><input type="date" id="radate" value={1}></td>
-			</tr>
-			<tr>
-				<td>Arrival time:</td>
-				<td><input type="time" step="300" id="ratime" value={2}></td>
-			</tr>
-		</table>
-	</fieldset>
-	`.format(flight_counter, moment().format("YYYY-MM-DD"), "12:00")
-	
-	return flight_str
-}
-
-////////////////////////////////////
-// hotel
-////////////////////////////////////
-
-function addHotel(divName){
-
-  var newdiv = document.createElement('div');
-  newdiv.innerHTML = getHotelHTML();
-  document.getElementById(divName).appendChild(newdiv);
-  hotel_counter++;
-  
-  focusFirstInput(newdiv);
-}
-
-function getHotelHTML(){	
-	var hotel_str = `<div id="div_hotel_{0}">
-	<fieldset>
-    <legend>Hotel {0}</legend>
-		<table border=0 id="hotel_{0}">
-			<tr>
-				<td>Hotel description:</td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>Base price:</td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>Taxes and fees:</td>
-				<td><input type="text" id="taxes"></td>
-			</tr>
-			<tr>
-				<td>&nbsp;</td>
-				<td><input type="button" value="Remove hotel" onClick="removeElement(document.getElementById('div_hotel_{0}').id, 'hotel')"</td>
-			</tr>
-		</table>
-	</fieldset>
-	`.format(hotel_counter)
-	
-	return hotel_str
-}
-
-
-///////////////////////////////////
-// generateReport
-////////////////////////////////////
-
-function generateReport(){
-	//globals
-	var customer = document.getElementById("customer_name").value;
-	var package = document.getElementById("package").value;
-	var accommodation_desc = document.getElementById("acco_desc").value;
-	NO_ADULTS = document.getElementById("adults").value;
-	NO_OF_NIGHTS = document.getElementById("nights").value;
-	var plural = ""
-	if (NO_OF_NIGHTS > 1){
-		plural = "s";
-	}
-	
-	var tables = document.getElementsByTagName('table');
-    var txt = "";
-	var report_objects = [];
-    for (var i = 0; i < tables.length; i++) {
-		//switch on type
-		var cur_id = tables[i].id;
-		var cat_cur_id = cur_id.split("_")
-		cur_txt = ""
-		var cur_date_str = getDateElement(tables[i], cat_cur_id[0])
-		var cur_time_str = getTimeElement(tables[i], cat_cur_id[0])
-		switch(cat_cur_id[0]){
-			case 'flight': 
-				cur_txt = reportFlight(tables[i]);		
-				break;
-			case 'hotel': 
-				cur_txt = reportHotel(tables[i]);
-				break;			
-		}
-		
-		var cur_date_time = null;
-		if(cat_cur_id[0] != "hotel"){
-			cur_date_time = moment(cur_date_str);
-			//TODO: check on pamelas PC how a string is stored
-			var split_time = cur_time_str.split(":");
-			cur_date_time.hour(parseInt(split_time[0]));
-			cur_date_time.minute(parseInt(split_time[1]));
-		}
-
-		var obj = {
-			id : i,
-			category : cat_cur_id[0],
-			content  : cur_txt,
-			date : moment(cur_date_str),
-			date_str : cur_date_str,
-			time     : cur_date_time
-		};		
-		
-		txt = txt + cur_txt + "</br>";
-		report_objects.push(obj);
-    }
-	
-	//sort date strings
-	report_objects.sort(function(a, b) { 
-		return a.date - b.date;
-	})
-	
-	//unique dates
-	var unique_dates = []
-	var date_groups = {}
-	for(var i=0; i < report_objects.length; i++){
-		
-	    if(!contains.call(unique_dates, report_objects[i].date_str)){
-			date_groups["{0}".format(report_objects[i].date_str)] = [report_objects[i]];
-			unique_dates.push(report_objects[i].date_str)
-		}
-		else{
-			date_groups["{0}".format(report_objects[i].date_str)].push(report_objects[i])
-		}
-	}
-	
-	//re-order each date_group
-	for(prop in date_groups){
-		if(date_groups[prop].length > 1){
-			date_groups[prop] = sortGroupByCategory(date_groups[prop]);
-		}
-	}
-	
-	//assemble output in right order
-	var right_order_txt = ""
-	for(prop in date_groups){
-		for(var i=0; i < date_groups[prop].length; i++){
-			right_order_txt = right_order_txt + date_groups[prop][i].content + "</br>";
-		}
-	}
-
-	//Prepend generat information:
-	var gen_info_template = "Hello {0},</br></br>Thanks for your message, I'd be happy to help you book this trip!</br></br> \
-		Our {1} includes: \
-		<ul><li>round trip flights from {2} to {3}</li>\
-		    <li>{4} accommodation{5} in {6}</li>\
-		    <li>round trip transfers between airport and hotel</li></ul></br>".format(customer, package, ORIGIN_CITY, DEST_CITY, NO_OF_NIGHTS, plural, accommodation_desc)
-	
-	var post_stc = "Let me know what you think of these, and I'm happy to look up some more options. Thanks!"
-	right_order_txt = gen_info_template + right_order_txt + post_stc;
-	
-	document.getElementById("resultArea").innerHTML = right_order_txt;
-}
-
 function sortGroupByCategory(group){
-	
 	var full_group = group.slice();
 	var hotel_idxs = []
 	var hotel_group = [];
@@ -395,6 +193,228 @@ function getTimeElement(node, category){
 			break; 
 	}
 	return time;
+}
+
+/*
+ * HTML form elements
+ */
+
+////////////////////////////////////
+// flight
+////////////////////////////////////
+
+function addFlight(divName){
+  	var newdiv = document.createElement('div');
+  	newdiv.innerHTML = getFlightHTML();
+  	document.getElementById(divName).appendChild(newdiv);
+  	focusFirstInput(newdiv);
+	flight_counter++;
+}
+
+function getFlightHTML(){
+
+	NO_OF_NIGHTS = document.getElementById("nights").value;
+
+	var flight_str = `<div id="div_flight_{0}">
+	<fieldset>
+    <legend>Flight</legend>
+		<table border=0 id="flight_{0}">
+			<tr>
+				<td>Departure city:</td>
+				<td><input type="text"></td>
+			</tr>
+			<tr>
+				<td>Departure date:</td>
+				<td><input type="date" id="ddate" value={1}></td>
+			</tr>
+			<tr>
+				<td>Departure time:</td>
+				<td><input type=time step="300" id="dtime" value={2}></td>
+			</tr>
+			<tr>
+				<td>Arrival city:</td>
+				<td><input type="text"></td>
+			</tr>
+			<tr>
+				<td>Arrival date:</td>
+				<td><input type="date" id="adate" value={1}></td>
+			</tr>
+			<tr>
+				<td>Arrival time:</td>
+				<td> <input type="time" step="300" id="atime" value={2}></td>
+			</tr>
+			<tr>
+				<td><b>Return flight</td>
+			</tr>
+			<tr>
+				<td>Departure date:</td>
+				<td><input type="date" id="rddate" value={3}></td>
+			</tr>
+			<tr>
+				<td>Departure time:</td>
+				<td><input type=time step="300" id="rdtime" value={2}></td>
+			</tr>
+			<tr>
+				<td>Arrival date:</td>
+				<td><input type="date" id="radate" value={3}></td>
+			</tr>
+			<tr>
+				<td>Arrival time:</td>
+				<td><input type="time" step="300" id="ratime" value={2}></td>
+			</tr>
+		</table>
+	</fieldset>
+	`.format(flight_counter, moment().format("YYYY-MM-DD"), "12:00", moment().add(NO_OF_NIGHTS, 'days').format("YYYY-MM-DD"))
+	
+	return flight_str
+}
+
+////////////////////////////////////
+// hotel
+////////////////////////////////////
+
+function addHotel(divName){
+  var newdiv = document.createElement('div');
+  newdiv.innerHTML = getHotelHTML();
+  document.getElementById(divName).appendChild(newdiv);
+  hotel_counter++;
+  focusFirstInput(newdiv);
+}
+
+function getHotelHTML(){	
+	var hotel_str = `<div id="div_hotel_{0}">
+	<fieldset>
+    <legend>Hotel {0}</legend>
+		<table border=0 id="hotel_{0}">
+			<tr>
+				<td>Hotel description:</td>
+				<td><input type="text"></td>
+			</tr>
+			<tr>
+				<td>Base price:</td>
+				<td><input type="text"></td>
+			</tr>
+			<tr>
+				<td>Taxes and fees:</td>
+				<td><input type="text" id="taxes"></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><input type="button" value="Remove hotel" onClick="removeElement(document.getElementById('div_hotel_{0}').id, 'hotel')"</td>
+			</tr>
+		</table>
+	</fieldset>
+	`.format(hotel_counter)
+	
+	return hotel_str
+}
+
+
+/*
+ * report generation
+ */
+
+function generateReport(){
+
+	// init
+	var customer = document.getElementById("customer_name").value;
+	var package = document.getElementById("package").value;
+	var accommodation_desc = document.getElementById("acco_desc").value;
+	NO_ADULTS = document.getElementById("adults").value;
+	NO_OF_NIGHTS = document.getElementById("nights").value;
+	var plural = ""
+	if (NO_OF_NIGHTS > 1){
+		plural = "s";
+	}
+	
+	// gather elements
+	var tables = document.getElementsByTagName('table');
+    var txt = "";
+	var report_objects = [];
+    for (var i = 0; i < tables.length; i++) {
+		//switch on type
+		var cur_id = tables[i].id;
+		var cat_cur_id = cur_id.split("_")
+		cur_txt = ""
+		var cur_date_str = getDateElement(tables[i], cat_cur_id[0])
+		var cur_time_str = getTimeElement(tables[i], cat_cur_id[0])
+		switch(cat_cur_id[0]){
+			case 'flight': 
+				cur_txt = reportFlight(tables[i]);		
+				break;
+			case 'hotel': 
+				cur_txt = reportHotel(tables[i]);
+				break;			
+		}
+		
+		var cur_date_time = null;
+		if(cat_cur_id[0] != "hotel"){
+			cur_date_time = moment(cur_date_str);
+			//TODO: check on pamelas PC how a string is stored
+			var split_time = cur_time_str.split(":");
+			cur_date_time.hour(parseInt(split_time[0]));
+			cur_date_time.minute(parseInt(split_time[1]));
+		}
+
+		var obj = {
+			id : i,
+			category : cat_cur_id[0],
+			content  : cur_txt,
+			date : moment(cur_date_str),
+			date_str : cur_date_str,
+			time     : cur_date_time
+		};		
+		
+		txt = txt + cur_txt + "</br>";
+		report_objects.push(obj);
+    }
+	
+	//sort date strings
+	report_objects.sort(function(a, b) { 
+		return a.date - b.date;
+	})
+	
+	//unique dates
+	var unique_dates = []
+	var date_groups = {}
+	for(var i=0; i < report_objects.length; i++){
+		
+	    if(!contains.call(unique_dates, report_objects[i].date_str)){
+			date_groups["{0}".format(report_objects[i].date_str)] = [report_objects[i]];
+			unique_dates.push(report_objects[i].date_str)
+		}
+		else{
+			date_groups["{0}".format(report_objects[i].date_str)].push(report_objects[i])
+		}
+	}
+	
+	// re-order each date_group
+	for(prop in date_groups){
+		if(date_groups[prop].length > 1){
+			date_groups[prop] = sortGroupByCategory(date_groups[prop]);
+		}
+	}
+	
+	// assemble output in right order
+	var right_order_txt = ""
+	for(prop in date_groups){
+		for(var i=0; i < date_groups[prop].length; i++){
+			right_order_txt = right_order_txt + date_groups[prop][i].content + "</br>";
+		}
+	}
+
+	// prepend generat information:
+	var gen_info_template = "Hello {0},</br></br>Thanks for your message, I'd be happy to help you book this trip!</br></br> \
+		Our {1} includes: \
+		<ul><li>round trip flights from {2} to {3}</li>\
+		    <li>{4} accommodation{5} in {6}</li>\
+		    <li>round trip transfers between airport and hotel</li></ul></br>".format(customer, package, ORIGIN_CITY, DEST_CITY, NO_OF_NIGHTS, plural, accommodation_desc)
+	
+	// append end greeting
+	var post_stc = "Let me know what you think of these, and I'm happy to look up some more options. Thanks!"
+	right_order_txt = gen_info_template + right_order_txt + post_stc;
+	
+	document.getElementById("resultArea").innerHTML = right_order_txt;
 }
 
 function reportFlight(flight_node){
